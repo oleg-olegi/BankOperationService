f371b56f5a1ad8848c40836687dd854d22baef6e
oleg-olegi <ol.shink87@gmail.com>
2024-06-16 21:46:41 +0300

    changing by Kulin's review reformat TransferService + save state of an account

--- src/main/java/com/example/bankoperationservice/service/TransferService.java
+++ src/main/java/com/example/bankoperationservice/service/TransferService.java
@@ -2,12 +2,15 @@ package com.example.bankoperationservice.service;
 
 import com.example.bankoperationservice.exceptions.BankAccountNotFoundException;
 import com.example.bankoperationservice.exceptions.InsufficientFundsException;
+import com.example.bankoperationservice.model.BankAccount;
 import com.example.bankoperationservice.model.UserData;
 import com.example.bankoperationservice.repository.IBankAccountRepository;
 import com.example.bankoperationservice.repository.IUserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.math.BigDecimal;
 
@@ -21,24 +24,30 @@ public class TransferService {
 
     @Transactional
     public void transferMoney(String senderBankAccId, String recipientBankAccId, BigDecimal amount) {
-
         UserData sender = userRepository.findByBankAccountId(senderBankAccId).orElseThrow(() -> new BankAccountNotFoundException(
                 String.format("Sender's bank account with id '%s' not found", senderBankAccId)));
 
         UserData recipient = userRepository.findByBankAccountId(recipientBankAccId).orElseThrow(() -> new BankAccountNotFoundException(
                 String.format("Recipient's bank account with id '%s' not found", senderBankAccId)));
 
-        BigDecimal senderBalance = bankRepository.findById(sender.getBankAccount().getAccountNumber()).orElseThrow(
-                () -> new BankAccountNotFoundException(
-                        String.format("Sender's bank account with id '%s' not found", senderBankAccId))).getBalance();
+        BankAccount senderAccount = bankRepository.findById(sender.getBankAccount().getAccountNumber())
+                .orElseThrow(() -> new BankAccountNotFoundException(
+                        String.format("Sender's bank account with id '%s' not found", senderBankAccId)));
 
-        BigDecimal recipientBalance = bankRepository.findById(recipient.getBankAccount().getAccountNumber()).orElseThrow(
-                () -> new BankAccountNotFoundException(
-                        String.format("Recipient's bank account with id '%s' not found", senderBankAccId))).getBalance();
+        BankAccount recipientAccount = bankRepository.findById(recipient.getBankAccount().getAccountNumber())
+                .orElseThrow(() -> new BankAccountNotFoundException(
+                        String.format("Recipient's bank account with id '%s' not found", recipientBankAccId)));
+
+        BigDecimal senderBalance = senderAccount.getBalance();
+        BigDecimal recipientBalance = recipientAccount.getBalance();
+
         if (senderBalance.compareTo(amount) < 0) {
             throw new InsufficientFundsException("Недостаточно средств");
         }
         sender.getBankAccount().setBalance(senderBalance.subtract(amount));
         recipient.getBankAccount().setBalance(recipientBalance.add(amount));
+
+        bankRepository.save(senderAccount);
+        bankRepository.save(recipientAccount);
     }
 }
